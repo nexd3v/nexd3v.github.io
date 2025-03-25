000c82d4091efcec09d49f1549473b6a96e08634
nexd3v <andresvedoy@gmail.com>
2025-03-25 23:53:19 +0100

    wip

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+let button = document.getElementById("knapp");
+
+button.addEventListener("click", function(){
+    alert("hahaha");
+});
+
+const array = [];
+console.log(array);
+
+button.addEventListener("click", function(){
+    array.push(1,2,3);
+    console.log(array);
+});
