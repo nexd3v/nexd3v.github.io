ac437f775650ec813ed31118177858c9ac13d7b3
nexd3v <andresvedoy@gmail.com>
2025-04-01 00:07:51 +0200

    small updates

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,18 @@ button.addEventListener("click", function () {
   array.push(1, 2, 3);
   console.log(array);
 });
+
+let buttonX = document.getElementById("buttonx");
+const url = "http://x.com/_nexd3v";
+
+buttonX.addEventListener("click", function () {
+  window.open(url, "_blank");
+});
+
+const arrayX = [];
+console.log(arrayX);
+
+buttonX.addEventListener("click", function () {
+  arrayX.push(1, 2, 3);
+  console.log(arrayX);
+});
