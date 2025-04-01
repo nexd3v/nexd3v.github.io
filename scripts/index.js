e5a7f4804442e112dadac1c77c4ca037ce32f547
nexd3v <andresvedoy@gmail.com>
2025-04-01 23:27:31 +0200

    wip

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,48 @@
+// Welcome Splash Screen
+//const splash = document.querySelector(".splash");
+
+//document.addEventListener("DOMContentLoaded", (e) => {
+//  setTimeout(() => {
+//    splash.classList.add("display-none");
+//  }, 3000);
+//});
+
+// Intro to Website
+let intro = document.querySelector(".intro");
+let logo = document.querySelector(".logo-header");
+let logoSpan = document.querySelectorAll(".logo");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
+    setTimeout(() => {
+      logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add(".fade");
+        }, (idx + 1) * 50);
+      });
+    }, 2000);
+
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2300);
+  });
+});
+
+// Button Function
 let button = document.getElementById("knapp");
+const url = "https://youtu.be/g3jCAyPai2Y";
 
 button.addEventListener("click", function () {
-  alert("hahaha");
+  alert(
+    "馬鹿みたい 子供なのね \n 夢を追って傷ついて \n 嘘が下手なくせに 笑えない笑顔みせた \n I love youも ろくに言わない \n 口下手でほんまに不器用 \n なのになのにどうして サヨナラは言えたの \n だめだね だめよ だめなのよ \n あんたが 好きで好きすぎて \n どれだけ 強いお酒でも \n 歪まない思い出が 馬鹿みたい"
+  );
+  window.open(url, "_blank");
 });
 
 const array = [];
@@ -12,17 +53,18 @@ button.addEventListener("click", function () {
   console.log(array);
 });
 
-let buttonX = document.getElementById("buttonx");
-const url = "http://x.com/_nexd3v";
+// Unused Button
+//let buttonX = document.getElementById("buttonx");
+//const urlx = "http://x.com/_nexd3v";
 
-buttonX.addEventListener("click", function () {
-  window.open(url, "_blank");
-});
+//buttonX.addEventListener("click", function () {
+//  window.open(url, "_blank");
+//});
 
-const arrayX = [];
-console.log(arrayX);
+//const arrayX = [];
+//console.log(arrayX);
 
-buttonX.addEventListener("click", function () {
-  arrayX.push(1, 2, 3);
-  console.log(arrayX);
-});
+//buttonX.addEventListener("click", function () {
+//  arrayX.push(1, 2, 3);
+//  console.log(arrayX);
+//});
